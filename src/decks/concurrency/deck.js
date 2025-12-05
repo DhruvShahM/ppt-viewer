299f484e9d9c7559cdc0b3722342fda9108d3c94
DhruvShah <dhruvshah78223@gmail.com>
2025-12-05 10:47:42 +0530

    Restore deck: concurrency

---
+++ src/decks/concurrency/deck.js
@@ -0,0 +1,25 @@
+import TitleSlide from '../../slides/TitleSlide';
+import ConcurrencyVsParallelism from '../../slides/ConcurrencyVsParallelism';
+import Goroutines from '../../slides/Goroutines';
+import Channels from '../../slides/Channels';
+import BufferedChannels from '../../slides/BufferedChannels';
+import SelectStatement from '../../slides/SelectStatement';
+import SyncPackage from '../../slides/SyncPackage';
+import ContextPackage from '../../slides/ContextPackage';
+import WorkerPools from '../../slides/WorkerPools';
+import BestPractices from '../../slides/BestPractices';
+import Summary from '../../slides/Summary';
+
+export default [
+    TitleSlide,
+    ConcurrencyVsParallelism,
+    Goroutines,
+    Channels,
+    BufferedChannels,
+    SelectStatement,
+    SyncPackage,
+    ContextPackage,
+    WorkerPools,
+    BestPractices,
+    Summary
+];
